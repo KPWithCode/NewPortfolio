057ee0764f38692e0cbea528b9669e8aa67be599
Kyle Phillips <khillips375@yahoo.com>
2019-11-30 23:49:40 -0500

    Up to date

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -26,33 +26,37 @@ const Projects = () => {
                                 <p className="crimson" style={{ fontSize: '1.5em' }}>Hostel Landing Page</p>
                                 <SocialIcon url="https://github.com/KPWithCode/Hostelle" />
                             </div>
+                            <p className="crimson" style={{fontSize:'1.2em'}}>React, Bootstrap</p>
                         </div>
 
 
                         <div className="card shadow p-3 mb-5" style={{ width: '50%', backgroundColor: '#ffe8e4', borderColor: '#ffe8e4' }}>
                             <img className="card-img-top" style={{ height: '50%' }} src={require('../photos/BlogSite.png')} alt="Blog Site" />
                             <div className="card-body" style={{ height: '20%' }}>
-                                <p className="crimson" style={{ fontSize: '1.5em' }}>FullStack Blog Site</p>
+                                <p className="crimson" style={{ fontSize: '1.5em' }}> Blog Site</p>
                                 <SocialIcon url="https://github.com/KPWithCode/haven2" />
                             </div>
+                            <p className="crimson" style={{fontSize:'1.2em'}}>Crud based application. Node, Express, React & MySQL</p>
                         </div>
 
 
                         <div className="card shadow p-3 mb-5" style={{ width: '50%', backgroundColor: '#ffe8e4', borderColor: '#ffe8e4' }}>
                             <img className="card-img-top" style={{ height: '50%' }} src={require('../photos/PusherChat.png')} alt="Chatroom App" />
                             <div className="card-body" style={{ height: '20%' }}>
-                                <p className="crimson" style={{ fontSize: '1.5em' }}>Chatroom</p>
+                                <p className="crimson" style={{ fontSize: '1.5em' }}>Chatroom App</p>
                                 <SocialIcon url="https://github.com/KPWithCode/HavenChatApp" />
                             </div>
+                            <p className="crimson" style={{fontSize:'1.2em'}}>React based website using Pusher Api</p>
                         </div>
 
                         <div className="card shadow p-3 mb-5" style={{ width: '50%', backgroundColor: '#ffe8e4', borderColor: '#ffe8e4' }}>
                             {/* <img className="card-img-top" style={{ height: '60%' }} src={require('../media/YelpApp.mp4')} alt="Weather App" /> */}
                             <video controls autoPlay className="card-img-top" style={{ height: '50%' }} src={YelpApp} type="video/mp4" />
                             <div className="card-body" style={{ height: '20%' }}>
-                                <p className="crimson" style={{ fontSize: '1.5em' }}>React Native Food App </p>
+                                <p className="crimson" style={{ fontSize: '1.5em' }}>Food App </p>
                                 <SocialIcon url="https://github.com/KPWithCode/Foodie" />
                             </div>
+                            <p className="crimson" style={{fontSize:'1.2em'}}>React Native app utilizing the Yelp Api</p>
                         </div>
                     </div>
                 </div>
